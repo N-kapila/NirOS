dd8c8b59d29eddf46e6363d7fc739619e5050c67
Nirmal Kapilarathne <63421770+N-kapila@users.noreply.github.com>
2021-08-06 15:53:36 +0530

    get outputs (#4)

--- kmain.c
+++ kmain.c
@@ -1,5 +1,10 @@
-int kmain()
-{
-	return 0;
-}
+#include "io.h"
+#include "writer.h"
+#include "serial_port.h"
 
+void kmain(){
+	char ptr1[]="Hello! Welcome to NirOS";
+	char ptr2[]="Hello! I'm Nirmal!";
+	fb_write(24,ptr1);
+	serial_write(0x3F8,ptr2,20);
+}
