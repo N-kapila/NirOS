da7529a14e14cbea210306853202d53f65cfe691
Nirmal Kapilarathne <63421770+N-kapila@users.noreply.github.com>
2021-08-13 18:42:30 +0530

    Segmentation (#5)
    * get outputs
    
    * add segments

--- kmain.c
+++ kmain.c
@@ -1,8 +1,12 @@
 #include "io.h"
 #include "writer.h"
 #include "serial_port.h"
+#include "segmentation/memory_segments.h"
 
 void kmain(){
+
+	segments_install_gdt();
+	
 	char ptr1[]="Hello! Welcome to NirOS";
 	char ptr2[]="Hello! I'm Nirmal!";
 	fb_write(24,ptr1);
